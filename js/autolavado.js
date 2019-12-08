9a6bf4405949ac2ad38388c5baaf93e796366824
ManiiySosa <raybanss13@gmail.com>
2019-12-07 20:40:55 -0600

    autolavado 1.5

---
+++ js/autolavado.js
@@ -0,0 +1,96 @@
+$('#inicio').on('click', function(){
+    $('html,body').animate( {scrollTop: 0}, 1000);
+    
+    $('#inicio2').css("color", "white");
+    
+    $('#servicios2').css("color", "blue"); 
+    
+     $('#paquetes2').css("color", "blue");
+    
+     $('#contacto2').css("color", "#blue");
+    
+     $('#nosotros2').css("color", "#blue");
+    
+     $('#promociones2').css("color", "#blue");
+
+})
+
+$('#servicios').on('click', function(){
+    $('html,body').animate( {scrollTop: 440}, 1000);
+    
+     $('#inicio2').css("color", "blue");
+    
+    $('#servicios2').css("color", "white"); 
+    
+     $('#paquetes2').css("color", "#blue");
+    
+     $('#contacto2').css("color", "blue");
+    
+     $('#nosotros2').css("color", "blue");
+    
+     $('#promociones2').css("color", "blue");
+})
+
+$('#paquetes').on('click', function(){
+    $('html,body').animate( {scrollTop: 1300}, 1000);
+    
+    $('#inicio2').css("color", "blue");
+    
+    $('#servicios2').css("color", "blue"); 
+    
+     $('#paquetes2').css("color", "white");
+    
+     $('#contacto2').css("color", "blue");
+    
+     $('#nosotros2').css("color", "blue");
+    
+     $('#promociones2').css("color", "blue");
+})
+
+$('#contacto').on('click', function(){
+    $('html,body').animate( {scrollTop: 2770}, 1000);
+    
+    $('#inicio2').css("color", "blue");
+    
+    $('#servicios2').css("color", "blue"); 
+    
+     $('#paquetes2').css("color", "blue");
+    
+     $('#contacto2').css("color", "white");
+    
+     $('#nosotros2').css("color", "blue");
+    
+     $('#promociones2').css("color", "blue");
+})
+
+$('#nosotros').on('click', function(){
+    $('html,body').animate( {scrollTop: 3400}, 1000);
+    
+    $('#inicio2').css("color", "blue");
+    
+    $('#servicios2').css("color", "blue"); 
+    
+     $('#paquetes2').css("color", "blue");
+    
+     $('#contacto2').css("color", "blue");
+    
+     $('#nosotros2').css("color", "white");
+    
+     $('#promociones2').css("color", "blue");
+})
+
+$('#promociones').on('click', function(){
+    $('html,body').animate( {scrollTop: 4000}, 1000);
+    
+    $('#inicio2').css("color", "blue");
+    
+    $('#servicios2').css("color", "blue"); 
+    
+     $('#paquetes2').css("color", "blue");
+    
+     $('#contacto2').css("color", "blue");
+    
+     $('#nosotros2').css("color", "blue");
+    
+     $('#promociones2').css("color", "white");
+})
